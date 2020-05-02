8540fa5d46e69069a8703b45f58c7f42290e0674
TariqBkn <boukouyen.tariq@gmail.com>
2020-05-02 01:31:29 +0000

    Add Cart route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
-import { CarouselComponent } from './carousel/carousel.component';
+import { CartComponent } from './cart/cart.component';
 
 
 const routes: Routes = [
-  {path: "", component:HomePageComponent}
+  {path: "", component:HomePageComponent},
+  {path: "cart", component:CartComponent}
 ];
 
 @NgModule({
