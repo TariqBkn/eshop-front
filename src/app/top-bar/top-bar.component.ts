21e02d19969de8cb4c322a10ba7670d5468bb5bc
TariqBkn <boukouyen.tariq@gmail.com>
2020-05-02 01:33:02 +0000

    Add animation to top-bar component

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,9 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
-  styleUrls: ['./top-bar.component.css']
+  styleUrls: ['./top-bar.component.css'],
+  animations: [
+    trigger('fade', [
+      transition('void => *', [
+        style({
+          opacity: 0.3
+        }),
+        animate(800)
+    ])
+    ])
+  ]
 })
 export class TopBarComponent implements OnInit {
 
