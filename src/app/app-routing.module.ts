7d30fe60fc22b99d578bb7bb32b25178acab3fd0
TariqBkn <boukouyen.tariq@gmail.com>
2020-05-04 18:07:04 +0000

    Add routes to product-details and create-account components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { CartComponent } from './cart/cart.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
 
 
 const routes: Routes = [
   {path: "", component:HomePageComponent},
-  {path: "cart", component:CartComponent}
+  {path: "cart", component:CartComponent},
+  {path: "details", component:ProductDetailsComponent},
+  {path: "createAccount", component:CreateAccountComponent}
 ];
 
 @NgModule({
