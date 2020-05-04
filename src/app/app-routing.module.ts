e3bef294958ea203a5bb4f617d099d86d3f6fe74
TariqBkn <boukouyen.tariq@gmail.com>
2020-05-04 23:52:41 +0000

    Add login route and change CreateAccount route to signup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { CartComponent } from './cart/cart.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CreateAccountComponent } from './create-account/create-account.component';
+import { LoginComponent } from './login/login.component';
 
 
 const routes: Routes = [
   {path: "", component:HomePageComponent},
   {path: "cart", component:CartComponent},
   {path: "details", component:ProductDetailsComponent},
-  {path: "createAccount", component:CreateAccountComponent}
+  {path: "signup", component:CreateAccountComponent},
+  {path: "login", component:LoginComponent}
 ];
 
 @NgModule({
