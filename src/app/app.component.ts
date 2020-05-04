49de63b41a1841cc6c6a8a7da0834056cf834a75
TariqBkn <boukouyen.tariq@gmail.com>
2020-05-04 17:55:42 +0000

    Add ngx-spinner to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgxSpinnerService } from "ngx-spinner";
+import { timeout } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'eshop-front';
+  
+  constructor(private spinner: NgxSpinnerService){
+    this.spinner.show();
+   }
+
+  ngOnInit() {
+     
+    this.spinner.hide();
+  }
 }
