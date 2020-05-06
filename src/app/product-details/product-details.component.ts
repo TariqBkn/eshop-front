9141ecabadb4751f5ab5c5227d4f470d702ed267
TariqBkn <boukouyen.tariq@gmail.com>
2020-05-06 05:27:42 +0000

    Add ngx-spinner to product-details

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Product } from '../Models/Product'
+import { NgxSpinnerService } from 'ngx-spinner';
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
@@ -7,9 +8,15 @@ import { Product } from '../Models/Product'
 })
 export class ProductDetailsComponent implements OnInit {
 
-  constructor() { }
   @Input('immobilier') product : Product;
-  ngOnInit(): void {
+  
+  constructor(private spinner: NgxSpinnerService){
+    this.spinner.show();
+   }
+
+  ngOnInit() {
+    
+    this.spinner.hide();
   }
 
 }
