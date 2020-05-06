4613990d77e2575d91d6e4ebe9ffb2880a0df6ea
TariqBkn <boukouyen.tariq@gmail.com>
2020-05-06 22:54:37 +0000

    Create an interceptor that adds authorization token to outgoing requests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule }    from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS }    from '@angular/common/http';
+import { TokenInterceptorService } from './Services/interceptors/token-interceptor.service'
 import { JwtHelperService, JWT_OPTIONS  } from '@auth0/angular-jwt';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -59,7 +60,8 @@ import { LogoutModuleComponent } from './logout-module/logout-module.component';
    ],
   providers: [
     { provide: JWT_OPTIONS, useValue: JWT_OPTIONS },
-        JwtHelperService
+        JwtHelperService,
+    {provide: HTTP_INTERCEPTORS, useClass: TokenInterceptorService, multi: true}
   ],
   bootstrap: [AppComponent]
 })
