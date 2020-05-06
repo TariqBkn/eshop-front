e501b42934fedccddeea01fec91cf33676a1aec3
TariqBkn <boukouyen.tariq@gmail.com>
2020-05-06 22:30:22 +0000

    Rectify product-details component and comment spinner out of it

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,22 +1,22 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Product } from '../Models/Product'
 import { NgxSpinnerService } from 'ngx-spinner';
+import { environment } from '../../environments/environment'
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
-
-  @Input('immobilier') product : Product;
-  
+  products_images_base_url=environment.main_api_url+"products/images/nature_work.jpg";
+  @Input('product') product : Product = new Product();
   constructor(private spinner: NgxSpinnerService){
-    this.spinner.show();
-   }
+    //this.spinner.show();
+  }
 
   ngOnInit() {
-    
-    this.spinner.hide();
+     
+    //this.spinner.hide();
   }
 
 }
