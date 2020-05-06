2d7a0790cf63627119716b8f1a5753cffcc9fdfc
TariqBkn <boukouyen.tariq@gmail.com>
2020-05-06 00:15:11 +0000

    Add logout route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,17 @@ import { CartComponent } from './cart/cart.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CreateAccountComponent } from './create-account/create-account.component';
 import { LoginComponent } from './login/login.component';
+import { AuthGuardService } from './Services/guards/auth-guard.service';
+import { LogoutModuleComponent } from './logout-module/logout-module.component';
 
 
 const routes: Routes = [
-  {path: "", component:HomePageComponent},
-  {path: "cart", component:CartComponent},
-  {path: "details", component:ProductDetailsComponent},
+  {path: "", component:HomePageComponent, canActivate:[AuthGuardService]},
+  {path: "cart", component:CartComponent, canActivate:[AuthGuardService]},
+  {path: "details", component:ProductDetailsComponent, canActivate:[AuthGuardService]},
   {path: "signup", component:CreateAccountComponent},
-  {path: "login", component:LoginComponent}
+  {path: "login", component:LoginComponent},
+  {path: "logout", component:LogoutModuleComponent, canActivate:[AuthGuardService]}
 ];
 
 @NgModule({
