c68214a108c7a9aadee80e56366da3b084d37d5a
TariqBkn <boukouyen.tariq@gmail.com>
2020-05-10 02:11:20 +0000

    Remove debugging alerts from token-interceptor

--- src/app/Services/interceptors/token-interceptor.service.ts
+++ src/app/Services/interceptors/token-interceptor.service.ts
@@ -10,17 +10,14 @@ export class TokenInterceptorService implements HttpInterceptor {
   constructor(private authenticationService: AuthenticationService) {}
 
   intercept(request : HttpRequest<any>, next: HttpHandler) {
-    alert(request.url)
-     if( !request.url.match("/users/signup") && !request.url.match("users/login") ){
-      alert('authorized req') 
-      let authorizedReq = request.clone({
+      if( !request.url.match("/users/signup") && !request.url.match("users/login") ){
+        let authorizedReq = request.clone({
         setHeaders:{
           Authorization: 'Bearer '+this.authenticationService.getToken()
         }
       })
       return next.handle(authorizedReq);
     }
-    alert("public req")
-    return next.handle(request);
+     return next.handle(request);
   }
 }
