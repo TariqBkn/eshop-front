b5e5e250e93fdb6c01373b633658301a75f5a1f7
TariqBkn <boukouyen.tariq@gmail.com>
2020-05-10 02:16:35 +0000

    Create methods to retrieve products in product service

--- src/app/Services/products/products.service.ts
+++ src/app/Services/products/products.service.ts
@@ -2,11 +2,16 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 import { AuthenticationService } from '../Authentication/authentication-service.service'
+import { Product } from 'src/app/Models/Product';
+import { Observable } from 'rxjs/internal/Observable';
+import { SimilarProduct } from 'src/app/Models/SimilarProduct';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
+
+
   token: string;
   userId: string;
    
@@ -14,21 +19,29 @@ export class ProductsService {
     headers: new HttpHeaders()
   }
 
-  productsBaseUrl: string = environment.main_api_url+"/products";
+  productsBaseUrl: string = environment.main_api_url+"/products/";
 
   constructor(
               private httpClient : HttpClient,
               private authenticationService:AuthenticationService,
-              ) { 
+               ) { 
                 this.authenticationService.currentUserData.subscribe(data=>{
                   if(data) {
                     this.token=data.token;
                     this.httpOptions.headers=new HttpHeaders({
-                      Authorization : 'Bearer '+this.token,
-                      'Content-Type':'application/json'
+                       'Content-Type':'application/json'
                       })
                   }
                 });
               }
 
+  getProductById(id: number):Observable<any>{
+      return this.httpClient.get<Product>(this.productsBaseUrl+id, this.httpOptions);
+  }
+  getProducts() {
+    return this.httpClient.get<any>(this.productsBaseUrl, this.httpOptions);
+  }
+  getSimilarProducts(productId: number) {
+    if(productId) return this.httpClient.get<any>(this.productsBaseUrl+productId+"/similar", this.httpOptions);
+  }
 }
