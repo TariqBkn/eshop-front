c06e1443d241ebe0d88e4e82f0dbfd00901e9d24
TariqBkn <boukouyen.tariq@gmail.com>
2020-05-10 02:15:09 +0000

    Add images data type and promotionRatio & quantityInStock variables in Product Model

--- src/app/Models/Product.ts
+++ src/app/Models/Product.ts
@@ -1,10 +1,14 @@
+import { Image } from './Image';
+
 export class Product {
     id:number;
     title:string;
     description:string;
     unitPrice:number;
     colors:[];
-    images: [""];
+    images: Image[];
     providerName:string;
     note:number;
+    promotionRatio:number;
+    quantityInStock:number;
 }
