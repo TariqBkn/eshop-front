beff394012334c46a5a8ea8eda2778b06e815ca7
TariqBkn <boukouyen.tariq@gmail.com>
2020-05-11 08:19:22 +0000

    Add number of order lines in cart badge to cart icon

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , OnChanges } from '@angular/core';
 import { fade, slide } from '../animations';
 import { FormBuilder  } from '@angular/forms';
 import { AuthenticationService } from '../Services/Authentication/authentication-service.service';
+import { NumberOfOrderLinesInCartService } from '../Services/DataShare/number-of-order-lines-in-cart.service';
+import { OrderLinesService } from '../Services/orderLines/order-lines.service';
+import { NotificationService } from '../Services/Notifications/notifications.service';
 
 @Component({
   selector: 'app-top-bar',
@@ -14,13 +17,33 @@ import { AuthenticationService } from '../Services/Authentication/authentication
 })
 export class TopBarComponent implements OnInit {
   authenticationService
-  constructor(authenticationService: AuthenticationService) {
+  constructor(authenticationService: AuthenticationService, private numberOfOrderLinesInCartService : NumberOfOrderLinesInCartService, private orderLinesService: OrderLinesService, private notificationService: NotificationService ) {
     this.authenticationService=authenticationService;
    }
-
-  ngOnInit(): void {
+    numberOfProductsInCart:number=0
+    
+    ngOnInit(): void {
+      this.subscribeToNumberOfOrderLinesInCartService();
+    }
+    private loadOrderLinesOfConnectedUser() {
+      this.orderLinesService.getOrderLines().subscribe(response => {
+         if(response) this.numberOfOrderLinesInCartService.pushValue(response.length) 
+      }, error => {
+        this.notificationService.warn("Erreur de connexion au serveur (header).");
+      });
+    }
+    OnChanges(){
+      this.loadOrderLinesOfConnectedUser()
+    }
+  private subscribeToNumberOfOrderLinesInCartService() {
+    this.numberOfOrderLinesInCartService.currentValue.subscribe(value => { this.numberOfProductsInCart = value; });
   }
+
     isUserLoggedIn(): boolean{
       return this.authenticationService.isUserLoggedIn()
+    }
+
+    noProductsInCart(){
+      return this.numberOfProductsInCart==0
     }
 }
