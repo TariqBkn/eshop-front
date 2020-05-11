ebda684c2c5e2c7132cab9348f3347ba216562f6
TariqBkn <boukouyen.tariq@gmail.com>
2020-05-11 06:21:08 +0000

    Rectify OrderLine Model

--- src/app/Models/OrderLine.ts
+++ src/app/Models/OrderLine.ts
@@ -1,11 +1,16 @@
 import { Product } from './Product';
+import { Order } from './Order';
 
-export class OrderLine{
-    product:Product;
-    quantity:number;
+export class OrderLine{   
+    id: number;
+    product :Product;
+    order: Order;
+    quantity: number;
+    cost: number;
 
     constructor(product:Product, quantity:number){
         this.product=product
         this.quantity=quantity
+        this.cost=quantity*(product.unitPrice*(1-product.promotionRatio))
     }
 }
