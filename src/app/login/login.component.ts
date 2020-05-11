2504791713bc158897f6fd14b8869cc8b9601e8f
TariqBkn <boukouyen.tariq@gmail.com>
2020-05-11 06:49:53 +0000

    Remove error message alert from login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -49,8 +49,7 @@ export class LoginComponent implements OnInit {
         }else if(error.message=="Http failure response for http://localhost:8080/login: 401 OK"){
           this.notificationService.warn("Email ou mot-de-passe incorrectes.");
         }else{
-          alert(error.message)
-           this.notificationService.warn("Erreur inconnue.");
+            this.notificationService.warn("Erreur inconnue.");
         }
           this.invalidLogin = true
       }
