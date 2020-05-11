ebaf8db4678f89dee751e5357575960fcab305f6
TariqBkn <boukouyen.tariq@gmail.com>
2020-05-11 06:37:06 +0000

    Finnish checkout.component.ts

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { OrdersService } from '../Services/orders/orders.service';
+import { NotificationService } from '../Services/Notifications/notifications.service';
+import { Router } from '@angular/router';
+import { Order } from '../Models/Order';
 
 @Component({
   selector: 'app-checkout',
@@ -8,18 +12,42 @@ import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 })
 export class CheckoutComponent implements OnInit {
 
+  @Input() orderId: number
+  order: Order= new Order()
   isLinear = true;
-  firstFormGroup: FormGroup;
-  secondFormGroup: FormGroup;
-
-  constructor(private formBuilder: FormBuilder) {}
+  payementMethodFormGroup: FormGroup;
+  orderCheckedOut=false
+  constructor(private formBuilder: FormBuilder, private ordersService: OrdersService, private notificationService: NotificationService, private router: Router) {}
 
   ngOnInit() {
-    this.firstFormGroup = this.formBuilder.group({
-      firstCtrl: ['', Validators.required]
+    this.payementMethodFormGroup = this.formBuilder.group({
+      payementMethod: ['', Validators.required]
     });
-    this.secondFormGroup = this.formBuilder.group({
-      secondCtrl: ['', Validators.required]
-    });
+    //this.order = this.ordersService.getOrder(this.orderId) 
+    this.getProduct()
+  }
+  getProduct(){
+     this.ordersService.getOrder(this.orderId).subscribe(
+      response=>{
+         this.order=response
+      },
+      error=>{
+        console.error(JSON.stringify(error))
+        if(error.status==404) this.notificationService.warn("Produit introuvable!") 
+        this.notificationService.warn("Une erreur s'est produite lors du chargement du produit")
+      }
+    )
+  }
+  onCheckout(){
+    this.ordersService.checkout(this.orderId).subscribe(
+      response=>{
+        this.orderCheckedOut=true
+        this.notificationService.warn("Votre demande à été traité.")
+        this.router.navigate([''])
+      },
+      error=>{
+        this.notificationService.warn("Votre demande n'a pas pu être traitée.");
+      }
+    )
   }
 }
