e254443b198cb1e10fdb9290be7aaa764ff8b065
TariqBkn <boukouyen.tariq@gmail.com>
2020-05-12 03:57:14 +0000

    Remove getters from Address model

--- src/app/Models/Address.ts
+++ src/app/Models/Address.ts
@@ -1,23 +1,12 @@
 export class Address{
-     private city:string;
-    private streetName:string;
-    private number:number;
+     city:string;
+     streetName:string;
+     number:number;
 
     constructor( city, streetName, number){
          this.city=city;
         this.streetName=streetName;
         this.number=number;
     }
- 
-    getCity():string{
-    return this.city;  
-    }
-    
-    getStreetName():string{
-    return this.streetName;  
-    }
-    
-    getNumber():number{
-    return this.number;  
-    }
+
 }
