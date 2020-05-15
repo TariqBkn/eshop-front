d9f733c6e12afdac58f4f30b50685ae3a71bcc2d
TariqBkn <boukouyen.tariq@gmail.com>
2020-05-15 01:21:41 +0000

    create users-list component

---
+++ src/app/users-list/users-list.component.ts
@@ -0,0 +1,89 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../Models/User';
+import { UsersService } from '../Services/users/users.service';
+import { NotificationService } from '../Services/Notifications/notifications.service';
+import { AuthenticationService } from '../Services/Authentication/authentication-service.service';
+
+@Component({
+  selector: 'app-users-list',
+  templateUrl: './users-list.component.html',
+  styleUrls: ['./users-list.component.css']
+})
+export class UsersListComponent implements OnInit {
+
+  users : Array<User> = new Array<User>();
+  page:number=0;
+  pages : Array<number>;
+  userId:string;
+  value;
+  isWaiting;
+  constructor(private usersService : UsersService,
+              private notificationService : NotificationService,
+              private authenticationService : AuthenticationService) { }
+
+  ngOnInit() {
+    this.getUserData();
+    this.getUsers();
+  }
+
+  getUserData() {
+    this.authenticationService.currentUserData.subscribe(data=>{
+      if(data) {
+        this.userId=data.id;
+      }
+    });
+  }
+  resetValue(){
+    this.value='';
+    this.getUsers();
+  }
+  onSearchChange(key:string){
+    if(key.length==0){
+        this.getUsers();
+    }
+    this.isWaiting=true;
+    if(key.length>0) this.getSearchResults(key);
+    this.isWaiting=false;
+  }
+  getSearchResults(key:string) {
+    this.usersService.getUserbyKeyWord(key,this.page).subscribe(
+      result=>{
+        this.users=result['content'];
+        this.pages=new Array(result['totalPages']);
+      },
+      err=>{
+        alert(JSON.stringify(err));
+      }
+    );
+  }
+  getUsers() {
+    this.usersService.getUsers(this.page).subscribe(
+      users=>{
+          this.users=users['content'];
+          this.pages=new Array(users['totalPages']);
+      },
+      err=>{
+          this.notificationService.warn("Erreur inconnue.")
+      });
+  }
+
+  
+    
+pageChanged(i:number,event:any){
+    event.preventDefault();
+    this.page=i;
+    this.getUsers();
+}
+switchLocked(userId){
+ this.usersService.alterAccountUnlocked(userId).subscribe(r=>{
+    if(r.message=="OK"){
+      this.notificationService.success("Effectué")
+      var u = this.users.find(x=>x.id == userId);
+      u.accountNonLocked=!u.accountNonLocked;
+    }else{
+      this.notificationService.warn("Erreur inconnue");
+    }
+  });
+}
+
+}
