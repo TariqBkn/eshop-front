89ef63af4012e37963797a340d05954d31bae704
TariqBkn <boukouyen.tariq@gmail.com>
2020-05-16 05:17:42 +0000

    Add hasAtLeastMaxNumberOfImages method to Product Model

--- src/app/Models/Product.ts
+++ src/app/Models/Product.ts
@@ -1,18 +1,23 @@
 import { Image } from './Image';
 
+
+const MAX_NUMBER_OF_IMAGES=3
+
 export class Product {
+   
     id:number;
     title:string;
     description:string;
     unitPrice:number;
-    colors:[];
-    images: Image[];
+    color:string;
+    images: Image[] = new Array();
     providerName:string;
-    note:number;
     promotionRatio:number;
     quantityInStock:number;
 
-
+    public hasAtLeastMaxNumberOfImages(){
+        return this.images.length>MAX_NUMBER_OF_IMAGES
+    }
     public getNameOfFirstImage(){
         if(this.images) return this.images[0].name;
     }
